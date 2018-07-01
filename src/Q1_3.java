ba9eae7b4628cf25c9f0f6f4968dcc4baf846b3a
zx <sure.zhang@gmail.com>
2018-07-01 14:17:59 +0800

    Q1.3 init

---
+++ src/Q1_3.java
@@ -0,0 +1,23 @@
+public class Q1_3 {
+    static public char[] URLify(char[] str) {
+        int blankCount = 0;
+        int length = 0;
+        for(;str[length] != '\0'; length++)
+            if (str[length] == ' ')
+                blankCount++;
+        int destIndex = length + blankCount*2;
+        for(int i = length-1;i>=0; i--) {
+            if (str[i] == ' ') {
+                destIndex -= 3;
+                str[destIndex] = '%';
+                str[destIndex+1] = '2';
+                str[destIndex+2] = '0';
+            }
+            else {
+                destIndex -= 1;
+                str[destIndex] = str[i];
+            }
+        }
+        return str;
+    }
+}
---
+++ tests/Q1_3Test.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class Q1_3Test {
+
+    @Test
+    public void URLify() {
+        myAssertArrayEquals(Q1_3.URLify(toChar("")), toChar(""));
+        myAssertArrayEquals(Q1_3.URLify(toChar(" ")), toChar("%20"));
+        myAssertArrayEquals(Q1_3.URLify(toChar("  ")), toChar("%20%20"));
+        myAssertArrayEquals(Q1_3.URLify(toChar(" ab")), toChar("%20ab"));
+        myAssertArrayEquals(Q1_3.URLify(toChar("ab ")), toChar("ab%20"));
+        myAssertArrayEquals(Q1_3.URLify(toChar("ab")), toChar("ab"));
+        myAssertArrayEquals(Q1_3.URLify(toChar("a")), toChar("a"));
+    }
+
+    private void myAssertArrayEquals(char[] chars, char[] chars1) {
+        int i;
+        for(i = 0; i < chars.length && chars[i] != '\0'; i++)
+        {
+            if (chars[i] != chars[i])
+                Assert.fail();
+        }
+        Assert.assertTrue(chars[i] == 0);
+    }
+
+    public char[] toChar(String c) {
+        char[] rc = c.toCharArray();
+        char[] rc2 = new char[rc.length*3];
+        System.arraycopy(rc, 0, rc2, 0, rc.length);
+        return rc2;
+    }
+}
