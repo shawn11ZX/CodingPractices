d508184e8fc5cd0ec8b5bf028a3a1de30e678a07
zx <sure.zhang@gmail.com>
2018-07-07 20:06:05 +0800

    Q1.8 add comments about other alternatives

--- src/Q1/Q1_8.java
+++ src/Q1/Q1_8.java
@@ -1,5 +1,16 @@
 package Q1;
 
+/**
+ * I originally came out with an naive algorithm with no extra space:
+ * - iterate through entire matrix, and change '0' to some non-existing value 'v'
+ * - 'v' can be calculated by finding the max value of elements of the matrix, 'v = m + 1'
+ * - iterate through entire matrix again, for each element with value 'v', set its row and col to zero.
+ *
+ * There is a more elegant algorithm:
+ * - store 0 mask at first row and col of the matrix: for each e[i,j] == 0, set row[0, j] = 0 and col[i, 0] = 0
+ * - since the first row and col will only be changed to zero when it should have been, there is no need to back it up before head.
+ * - of cause, we have to check row 0 and col 0 should be all set to zero at first.
+ */
 public class Q1_8 {
     static class Matrix {
         int _rows;
