c16a6c964687214d9b667c74274dcf786d7fdd3c
zx <sure.zhang@gmail.com>
2018-07-15 12:23:03 +0800

    Q2.4 init 2

--- src/Q2/Q2_4.java
+++ src/Q2/Q2_4.java
@@ -16,7 +16,27 @@ public class Q2_4 {
             int temp = cur.data;
             cur.data = target.data;
             target.data = temp;
-            cur = target.next;
+            cur = cur.next;
         } while(true);
+    }
+
+    public static ListNode partition2(ListNode head, int pivot) {
+        ListNode small = head;
+        ListNode large = head;
+        ListNode cur = head;
+        while(cur != null) {
+            ListNode next = cur.next;
+            if(cur.data >= pivot) {
+                large.next = cur;
+                large = cur;
+                cur.next = null;
+            }
+            else {
+                cur.next = small;
+                small = cur;
+            }
+            cur = next;
+        }
+        return small;
     }
 }
--- tests/Q2/Q2_4Test.java
+++ tests/Q2/Q2_4Test.java
@@ -8,10 +8,73 @@ public class Q2_4Test {
 
     @Test
     public void testPartation() {
-        ListNode input = new ListNode(3, new ListNode(5, new ListNode(8, new ListNode(5, new ListNode(10, new ListNode(2, new ListNode(1)))))));
-        ListNode expect = new ListNode(3, new ListNode(2, new ListNode(1, new ListNode(5, new ListNode(10, new ListNode(5, new ListNode(8)))))));
+        {
+            ListNode input = new ListNode(3, new ListNode(5, new ListNode(8, new ListNode(5, new ListNode(10, new ListNode(2, new ListNode(1)))))));
+            ListNode expect = new ListNode(3, new ListNode(2, new ListNode(1, new ListNode(5, new ListNode(10, new ListNode(5, new ListNode(8)))))));
 
-        Q2_4.partation(input, 5);
-        ListTestUtils.assertEquals(input, expect);
+            Q2_4.partation(input, 5);
+            ListTestUtils.assertEquals(input, expect);
+        }
+
+        {
+            ListNode input = new ListNode(3);
+            ListNode expect = new ListNode(3);
+
+            Q2_4.partation(input, 5);
+            ListTestUtils.assertEquals(input, expect);
+        }
+
+
+        {
+            ListNode input = new ListNode(3);
+            ListNode expect = new ListNode(3);
+
+            Q2_4.partation(input, 1);
+            ListTestUtils.assertEquals(input, expect);
+        }
+
+        {
+            ListNode input = null;
+            ListNode expect = null;
+
+            Q2_4.partation(input, 1);
+            ListTestUtils.assertEquals(input, expect);
+        }
+    }
+
+    @Test
+    public void testPartation2() {
+        {
+            ListNode input = new ListNode(3, new ListNode(5, new ListNode(8, new ListNode(5, new ListNode(10, new ListNode(2, new ListNode(1)))))));
+            ListNode expect = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(5, new ListNode(8, new ListNode(5, new ListNode(10)))))));
+
+            Q2_4.partition2(input, 5);
+            ListTestUtils.assertEquals(input, expect);
+        }
+
+        {
+            ListNode input = new ListNode(3);
+            ListNode expect = new ListNode(3);
+
+            Q2_4.partition2(input, 5);
+            ListTestUtils.assertEquals(input, expect);
+        }
+
+
+        {
+            ListNode input = new ListNode(3);
+            ListNode expect = new ListNode(3);
+
+            Q2_4.partition2(input, 1);
+            ListTestUtils.assertEquals(input, expect);
+        }
+
+        {
+            ListNode input = null;
+            ListNode expect = null;
+
+            Q2_4.partition2(input, 1);
+            ListTestUtils.assertEquals(input, expect);
+        }
     }
 }
