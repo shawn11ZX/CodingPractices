987df9b2acdc006193762e95e94f6bda62cfe3c1
zx <zx@wooduan.com>
2018-10-09 10:45:22 +0800

    Q5.2 revised

--- src/Q5/Q5_2.java
+++ src/Q5/Q5_2.java
@@ -2,19 +2,19 @@ package Q5;
 
 public class Q5_2 {
     public static String printBinary(double d) {
-        if (d >= 1 || d < 0) {
+        if (d >= 1 || d <= 0) {
             return "ERROR";
         }
         StringBuilder sb = new StringBuilder();
         sb.append(".");
         while(d > 0) {
             d = d * 2;
-            if (d > 1) {
+            if (d >= 1) {
                 sb.append("1");
                 d = d - 1;
             }
             else {
-                sb.append("0")
+                sb.append("0");
             }
         }
         return sb.toString();
---
+++ tests/Q5/Q5_2Test.java
@@ -0,0 +1,13 @@
+package Q5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q5_2Test {
+    @Test
+    public void test() {
+        Q5_2.printBinary(0.77);
+        Q5_2.printBinary(0);
+    }
+}
