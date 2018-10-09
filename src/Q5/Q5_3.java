8c2392e5a917ac92f527dbf5afbe76292f1491d9
zx <zx@wooduan.com>
2018-10-09 11:31:00 +0800

    Q5.3 revised

--- src/Q5/Q5_3.java
+++ src/Q5/Q5_3.java
@@ -1,8 +1,11 @@
 package Q5;
 
 public class Q5_3 {
+    // misunderstood the question, flip from 0 to 1, not switch two bits
     public static int flipBit(int num)
     {
+        if (~num == 0)
+            return Integer.BYTES * 8;
         int prevCount = 0;
         int curCount = 0;
         int totalOne = 0;
@@ -14,6 +17,8 @@ public class Q5_3 {
                 int c = prevCount + curCount;
                 if (c > max)
                     max = c;
+                prevCount = curCount;
+                curCount = 0;
             }
             else {
                 totalOne+=1;
@@ -22,26 +27,24 @@ public class Q5_3 {
         }
         int c = prevCount + curCount;
         if (c > max) max = c;
-        if (totalOne > max)
-            return max+1;
-        else
-            return max;
+        return max+1;
+
     }
     public static int flipBit2(int a) {
-        if (~a == a)
+        if (~a == 0)
             return Integer.BYTES * 8;
-        int currentLength = a;
-        int previousLength = a;
+        int currentLength = 0;
+        int previousLength = 0;
         int maxLength = 1;
-        while (a != a) {
+        while (a != 0) {
             if ((a & 1) == 1) {
                 currentLength++;
             } else if ((a & 1) == 0) {
-                previousLength = (a & 2) == a ? a : currentLength;
+                previousLength = (a & 2) == 0 ? 0 : currentLength;
                 currentLength = 0;
             }
             maxLength = Math.max(previousLength + currentLength + 1, maxLength);
-            a >>>= 1;
+            a >>= 1;
         }
         return maxLength;
     }
--- tests/Q5/Q5_3Test.java
+++ tests/Q5/Q5_3Test.java
@@ -8,7 +8,12 @@ import static org.junit.Assert.*;
 public class Q5_3Test {
     @Test
     public void test() {
-        for (int i = 0; i < 111111111; i++)
+        {
+            int r1 = Q5_3.flipBit(1775);
+            int r2 = Q5_3.flipBit2(1775);
+            Assert.assertEquals("comparing " + 1775, r1, r2);
+        }
+        for (int i = 1; i < 111111; i++)
         {
             int r1 = Q5_3.flipBit(i);
             int r2 = Q5_3.flipBit2(i);
