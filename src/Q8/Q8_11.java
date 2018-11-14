b6126dcdcbd5d0831260eefebb0edcbfa6d6fa35
zx <zx@wooduan.com>
2018-11-14 18:03:38 +0800

    Q8.11 init

---
+++ src/Q8/Q8_11.java
@@ -0,0 +1,48 @@
+package Q8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q8_11 {
+    public static int permChanges(int n, int [] changes, int cur, Map m)
+    {
+        if (n == 0)
+            return 1;
+        if (m.containsKey(n))
+            return (Integer) m.get(n);
+        int change = changes[cur];
+        if (cur == changes.length-1)
+            return ((n%change) == 0) ? 1: 0;
+        int types = n / change;
+        int rc = 0;
+        for(int i = 0; i <= types; i++)
+            rc += permChanges(n-i*change, changes, cur+1, m);
+        m.put(n, rc);
+        return rc;
+    }
+    public static int makeChange2(int n) {
+        int[] denoms = {25, 10, 5, 1};
+        return permChanges(n, denoms , 0, new HashMap());
+    }
+
+    public static int makeChange(int n) {
+        int[] denoms = {25, 10, 5, 1};
+        int[][] map = new int[n + 1][denoms.length];
+        return makeChange(n, denoms, 0, map);
+    }
+    public static int makeChange(int amount, int[] denoms, int index, int[][] map) {
+        if (map[amount][index] > 0) {
+            return map[amount][index];
+        }
+        if (index >= denoms.length - 1)
+            return 1;
+        int denomAmount = denoms[index];
+        int ways = 0;
+        for (int i  = 0; i * denomAmount <= amount; i++) {
+            int amountRemaining = amount - i * denomAmount;
+            ways += makeChange(amountRemaining, denoms, index +  1, map);
+        }
+        map[amount][index] = ways;
+        return ways;
+    }
+}
---
+++ tests/Q8/Q8_11Test.java
@@ -0,0 +1,24 @@
+package Q8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q8_11Test {
+
+    @Test
+    public void makeChange2() {
+        for(int i = 0; i < 100; i++)
+        {
+            int exp = Q8_11.makeChange(i);
+            int real = Q8_11.makeChange2(i);
+
+            Assert.assertEquals("index " + i, exp, real);
+        }
+    }
+
+    @Test
+    public void makeChange() {
+    }
+}
