e4113005ec69eab7590027f7eb2649edbb3797bd
zx <zx@wooduan.com>
2018-11-15 10:43:53 +0800

    Q8.10 revised

--- src/Q8/Q8_10.java
+++ src/Q8/Q8_10.java
@@ -6,58 +6,22 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Q8_10 {
-    public static class Point
+
+
+    public static enum Color
     {
-        int x;
-        int y;
+        White, Black
     }
-
-    public static class Image
-    {
-        int width;
-        int height;
-
-
-        public int getWidth() {
-            return width;
+    public static void fillColor(int x, int y, Color[][] image, Color oldColor, Color newColor) {
+        if (x < 0 || y < 0 || x >= image[0].length || y >= image.length)
+            return;
+        if (image[x][y] == oldColor) {
+            image[x][y] = newColor;
+            fillColor(x - 1, y, image, oldColor, newColor);
+            fillColor(x + 1, y, image, oldColor, newColor);
+            fillColor(x, y - 1, image, oldColor, newColor);
+            fillColor(x, y + 1, image, oldColor, newColor);
         }
 
-        public int getHeight() {
-            return height;
-        }
-
-        public Color getColor(Point start) {
-            return null;
-        }
-
-        public void setColor(Point p, Color color) {
-        }
-
-        public LinkedList<Point> getNeighbors(Point p) {
-            return null;
-        }
-    }
-
-    public static class Color
-    {
-    }
-    public static void fillColor(Point start, Image image, Color color)
-    {
-        HashMap visited = new HashMap();
-        LinkedList<Point> todo = new LinkedList();
-        todo.add(start);
-        Color oldColor = image.getColor(start);
-        while(!todo.isEmpty())
-        {
-            Point p = todo.removeLast();
-            image.setColor(p, color);
-            visited.put(p, true);
-            LinkedList<Point> l = image.getNeighbors(p);
-            for(Point n : l) {
-                Color nc = image.getColor(n);
-                if (nc.equals(oldColor) && !visited.containsKey(nc))
-                    todo.add(p);
-            }
-        }
     }
 }
