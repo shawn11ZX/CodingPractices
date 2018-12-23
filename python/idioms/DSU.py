200ffc9da77d9553cc02a5279b178797481a1ead
zx <sure.zhang@gmail.com>
2018-12-23 10:24:02 +0800

    add DSU

---
+++ python/idioms/DSU.py
@@ -0,0 +1,14 @@
+class DSU:
+    def __init__(self, N):
+        self.p = [x for x in range(N)]
+
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, x, y):
+        xr = self.find(x)
+        yr = self.find(y)
+        self.p[xr] = yr
+
